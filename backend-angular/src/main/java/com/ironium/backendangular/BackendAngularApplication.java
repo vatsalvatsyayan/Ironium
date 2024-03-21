73e9f799ae7122fc2827ed23207258934caca156
Vatsal Vatsyayan <vatsaljackspicer@gmail.com>
2024-03-21 18:07:24 +0530

    Added Initial Backend logic

---
+++ backend-angular/src/main/java/com/ironium/backendangular/BackendAngularApplication.java
@@ -0,0 +1,13 @@
+package com.ironium.backendangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAngularApplication.class, args);
+	}
+
+}
---
+++ backend-angular/src/main/java/com/ironium/backendangular/entity/ProductCategory.java
@@ -0,0 +1,32 @@
+package com.ironium.backendangular.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="product_category")
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
